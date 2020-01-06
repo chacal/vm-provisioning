699ab0544d55fd3aa6cc7f080805aa25134ea490
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-01-06 19:54:45 +0200

    Move provision from cloud config to a separate script for easier re-use

---
+++ scripts/provision_common_base.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y net-tools sysstat htop tmux git wget curl less \
+    libnss-mdns tcpdump man iftop dnsutils ndisc6 avahi-daemon  \
+    open-vm-tools zsh stow
+
+sudo sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sudo dpkg-reconfigure --frontend=noninteractive locales
+sudo update-locale LANG=en_US.UTF-8
