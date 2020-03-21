1b1fcd94e8f2a647dbd2e5c0ab60b6151f1033f2
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-03-21 07:37:24 +0200

    Parameterize LXC resource

--- terraform/modules/proxmox_lxc/proxmox_lxc.tf
+++ terraform/modules/proxmox_lxc/proxmox_lxc.tf
@@ -1,31 +1,3 @@
-variable "cores" {
-  type = number
-  default = 1
-}
-
-variable "memory" {
-  type = number
-  default = 1024
-}
-
-variable "disk_size" {
-  type = number
-  default = 100
-}
-
-variable "hostname" {
-  type = string
-}
-
-variable "ip" {
-  type = string
-}
-
-variable "rootfs_archive" {
-  type = string
-  default = "debian-10.0-standard_10.0-1_amd64.tar.gz"
-}
-
 resource "random_id" "random_mac" {
   byte_length = 5
 }
@@ -41,24 +13,26 @@ resource "proxmox_lxc" "lxc" {
   start = "true"
 
   hostname = var.hostname
-  rootfs = "local-zfs:${var.disk_size}"
+  nameserver = var.nameserver
+  searchdomain = var.searchdomain
+  rootfs = "${var.storage}:${var.disk_size}"
 
   network {
     name = "eth0"
-    bridge = "vmbr0"
+    bridge = var.bridge
     ip = "${var.ip}/24"
     ip6 = "auto"
-    gw = "10.90.70.1"
+    gw = var.gateway
     hwaddr = local.mac
     firewall = "true"
     rate = 0
-    tag = 0
+    tag = var.vlan
     type = "veth"
   }
   ostemplate = "local:vztmpl/${var.rootfs_archive}"
   ostype = "debian"
   ssh_public_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEApxT0ZP4MsFQ7PC43ugWcGNbHLfXN3kqx0rMMhfGsKCxvhLTGYwveDFfIrimCWWCWg248oNR0jzoH5mKz/stidF8fsVubBegAJ32N/f2jJ6hHlnmCbRBCNlm1BL5Yz+YkMVWTMXa38ICaJhOncDwtZvzUqicc6b7GQmRZ4X7tQTUD91ln7t+7VSqEYXeCmvAL4fY8i2PMlWZoaN6FHmquJYH09w0Hu20Nz5SyfqjYk1vhKnDk93CouTcCR4zONcZdyip7b5qQAHfkcyCqZLvFgadedqsFwe2lu9WgpWATTrJUWReykDzk/jHMaivboXxLaVxwvAmF1CejmWV74VNUFw== jihartik@localhost"
-  storage = "local-zfs"
+  storage = var.storage
   target_node = "fujari"
   unprivileged = true
 
---
+++ terraform/modules/proxmox_lxc/variables.tf
@@ -0,0 +1,56 @@
+variable "cores" {
+  type = number
+  default = 1
+}
+
+variable "memory" {
+  type = number
+  default = 1024
+}
+
+variable "disk_size" {
+  type = number
+  default = 100
+}
+
+variable "hostname" {
+  type = string
+}
+
+variable "bridge" {
+  type = string
+  default = "vmbr0"
+}
+
+variable "ip" {
+  type = string
+}
+
+variable "gateway" {
+  type = string
+  default = "10.50.100.1"
+}
+
+variable "nameserver" {
+  type = string
+  default = "10.50.100.1"
+}
+
+variable "searchdomain" {
+  type = string
+  default = "chacal.fi"
+}
+
+variable "vlan" {
+  type = number
+}
+
+variable "rootfs_archive" {
+  type = string
+  default = "debian-10.0-standard_10.0-1_amd64.tar.gz"
+}
+
+variable "storage" {
+  type = string
+  default = "local-zfs"
+}
